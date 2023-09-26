8244cf2afbd527dcd1b1ef6fbeb5c1411dc2efa5
Arthur <yi@imtt.com.tw>
2023-09-26 11:47:45 +0800

    feat : test xSemaphore success

--- testModules/SD_writeAtSameTimeRTOS/SD_writeAtSameTimeRTOS.ino
+++ testModules/SD_writeAtSameTimeRTOS/SD_writeAtSameTimeRTOS.ino
@@ -30,90 +30,64 @@ void SDInit(){
 String writeMsgPath = "system_log.txt";
 String msgA;
 String msgB;
-bool replaceFlag;
-bool timeStampFlag;
+bool replaceFlag = false;
+bool timeStampFlag = false;
 String createFolderPath;
 String createFilePath;
 
 
+// bool isInCritical = false;
+SemaphoreHandle_t xSemaphore = xSemaphoreCreateMutex();
 portMUX_TYPE writeFile_mutex = portMUX_INITIALIZER_UNLOCKED;
 
 void writeMsgToPath(String path, String msg, bool replace = false, bool timeStamp = true){
-
-  taskENTER_CRITICAL(&writeFile_mutex);
+  // taskENTER_CRITICAL(&writeFile_mutex);
+  // isInCritical = !isInCritical;
   Serial.println("write file");
-  FsFile tempfile;
-  
-  if(replace){
+
+  // wait for release
+  if(xSemaphoreTake( xSemaphore, portMAX_DELAY ) == pdTRUE){
+    FsFile tempfile;
     
-    int isOpened = tempfile.open(path.c_str(), O_WRONLY | O_CREAT);
-
-    if (!isOpened) {     // open need char array, not string. So use c_str to convert
-      Serial.println(" --> open " + path + " failed");
-      showErrorLed();
-      while(1){
-        delay(1000);
-      }
-    }else{
-      tempfile.println(msg.c_str());
-    }
-  }else{
-
-    int isOpened = tempfile.open(path.c_str(), O_WRONLY | O_CREAT | O_APPEND);
-
-    if (!isOpened) {     // open need char array, not string. So use c_str to convert
-      Serial.println(" --> open " + path + " failed");
-      showErrorLed();
-      while(1){
-        delay(1000);
-      }
-    }else{
-
-      tempfile.println(msg.c_str());
+    if(replace){
       
+      int isOpened = tempfile.open(path.c_str(), O_WRONLY | O_CREAT);
+
+      if (!isOpened) {     // open need char array, not string. So use c_str to convert
+        Serial.println(" --> open " + path + " failed");
+        showErrorLed();
+        while(1){
+          delay(1000);
+        }
+      }else{
+        tempfile.println(msg.c_str());
+      }
+    }else{
+
+      int isOpened = tempfile.open(path.c_str(), O_WRONLY | O_CREAT | O_APPEND);
+
+      if (!isOpened) {     // open need char array, not string. So use c_str to convert
+        Serial.println(" --> open " + path + " failed");
+        showErrorLed();
+        while(1){
+          delay(1000);
+        }
+      }else{
+
+        tempfile.println(msg.c_str());
+        
+      }
     }
+
+    // isInCritical = !isInCritical;
+    // taskEXIT_CRITICAL(&writeFile_mutex);
+
   }
 
-  taskEXIT_CRITICAL(&writeFile_mutex);
+  xSemaphoreGive( xSemaphore );
+  Serial.println("finished");
 }
 
-
-// void checkAndCreateFolder(String path){
-//   if (!sd.exists(path)) {
-//     sd.mkdir(path);
-//     if (sd.exists(systemLogPath)) {
-//       writeMsgToPath(systemLogPath, "folder at path : " + path + " create successful");
-//     }else{
-//       Serial.println("folder at path : " + path + " create successful");
-//     }
-//   }
-// }
-
-
-// void checkAndCreateFile(String path){
-//   FsFile createFile;
-//   if (!sd.exists(path)) {
-//     if (!createFile.open(path.c_str(), O_WRONLY | O_CREAT | O_APPEND)) {     // open need char array, not string. So use c_str to convert
-//       if (sd.exists(systemLogPath)) {
-//         writeMsgToPath(systemLogPath, "open " + path + " failed");
-//       }else{
-//         Serial.println("open " + path + " failed");
-//       }
-//     }else{
-//       createFile.close();
-//       if (sd.exists(systemLogPath)) {
-//         writeMsgToPath(systemLogPath, "file : " + path + " create successful");
-//       }else{
-//         Serial.println("file : " + path + " create successful");
-//       }
-//     }
-//   }
-// }
-
-
-// TaskHandle_t tFolder;
-// TaskHandle_t tFile;
-// TaskHandle_t tMSG;
 TaskHandle_t t0;
 TaskHandle_t t1;
 TaskHandle_t t2;
@@ -121,8 +95,6 @@ TaskHandle_t t3;
 
 bool needWriteMSGToPathA = false;
 bool needWriteMSGToPathB = false;
-// bool needCheckAndCreateFolder = false;
-// bool needCheckAndCreateFile = false;
 
 
 void checkNeedWriteMSGToPathA(void* pvParameters){
@@ -149,31 +121,6 @@ void checkNeedWriteMSGToPathB(void* pvParameters){
   }
 }
 
-
-// void checkNeedCheckAndCreateFolder(void* pvParameters){
-//   Serial.println("checkNeedCheckAndCreateFolder : created");
-//   while(1){
-//     if(needCheckAndCreateFolder){
-//       needCheckAndCreateFolder = !needCheckAndCreateFolder;
-//       needCheckAndCreateFolder(createFolderPath);
-//     }
-//     vTaskDelay(50);
-//   }
-// }
-
-
-
-
-// void checkNeedCheckAndCreateFile(void* pvParameters){
-//   Serial.println("checkNeedCheckAndCreateFile : created");
-//   while(1){
-//     if(needCheckAndCreateFile){
-//       needCheckAndCreateFile = !needCheckAndCreateFile;
-//       checkAndCreateFile(createFilePath);
-//     }
-//     vTaskDelay(50);
-//   }
-// }
 
 void createRequestA(void* pvParameters){
   Serial.println("createRequestA : created");
@@ -220,26 +167,6 @@ void createRTOSTasks() {
     &t1,                                 /* task handle */
     OTHER_TASK_CPU                          /* CPU core */
   );
-
-  // xTaskCreatePinnedToCore(
-  //   checkNeedCheckAndCreateFolder,                    /* Task function. */
-  //   "checkNeedCheckAndCreateFolder",           /* name of task. */
-  //   2048,                                   /* Stack size of task */
-  //   NULL,                                   /* parameter of the task */
-  //   2,                                      /* priority of the task */
-  //   &tFolder,                        /* task handle */
-  //   OTHER_TASK_CPU                          /* CPU core */
-  // );
-
-  // xTaskCreatePinnedToCore(
-  //   checkNeedCheckAndCreateFile,                    /* Task function. */
-  //   "checkNeedCheckAndCreateFile",           /* name of task. */
-  //   2048,                                   /* Stack size of task */
-  //   NULL,                                   /* parameter of the task */
-  //   2,                                      /* priority of the task */
-  //   &tFile,                        /* task handle */
-  //   OTHER_TASK_CPU                          /* CPU core */
-  // );
 
   xTaskCreatePinnedToCore(
     createRequestA,                    /* Task function. */
