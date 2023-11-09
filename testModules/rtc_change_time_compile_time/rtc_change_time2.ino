af139959b941463234103e04b8f6a7525bc8c7d6
Arthur <yi@imtt.com.tw>
2023-11-09 20:11:51 +0800

    feat : RTC change code

---
+++ testModules/rtc_change_time_compile_time/rtc_change_time2.ino
@@ -0,0 +1,47 @@
+/*--- 在這更改目標時間 ---*/
+#define YEAR      2023
+#define MONTH     11
+#define DAY       01
+#define HOUR_24   19
+#define MINUTE    30
+#define SECOND    00
+/*--- 在這更改目標時間 ---*/
+
+
+#include "RTClib.h"
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
+  Serial.begin(115200);
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  rtc.adjust(DateTime(__DATE__, __TIME__));
+}
+
+void loop () {
+    DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    delay(3000);
+}
