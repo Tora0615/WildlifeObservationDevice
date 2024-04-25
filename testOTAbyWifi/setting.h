42df05875143157370df95996c1a38190351ded2
Arthur <Yi@imtt.com.tw>
2024-04-25 16:39:24 +0800

    doc : add some info

--- testOTAbyWifi/setting.h
+++ testOTAbyWifi/setting.h
@@ -1,6 +1,7 @@
 #ifndef SETTING_H
 #define SETTING_H
 
+// This status will only keep when light/deep sleep (reset will clean the status)
 RTC_DATA_ATTR bool isFirstBoot = true;
 bool isNeedToUpdate = false;
 
